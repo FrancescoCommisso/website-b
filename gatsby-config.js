98d59ca5d18419ded91d2d82ccde254e1e8304f1
Francesco Commisso <francesco@Francescos-MacBook-Pro.local>
2020-07-20 21:38:48 -0400

    deleted config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-express",
-      options: {
-        output: "config/gatsby-express.json",
-      },
-    },
-
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `backgrounds`,
